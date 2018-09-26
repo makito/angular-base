01575ec2fac3769fcbd57bd6ea9c641244b259ff
Ivan Orekhov <makito82@gmail.com>
2018-09-26 09:50:53 +0300

    рефакторинг главного компонента приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Title } from '@angular/platform-browser';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterState, ActivatedRoute } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { has } from 'lodash';
@@ -10,7 +10,7 @@ import { has } from 'lodash';
   providers: [TranslatePipe],
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   private readonly _baseUrlTitle: string = 'angular-base';
 
@@ -18,19 +18,19 @@ export class AppComponent {
     private _translatePipe: TranslatePipe,
     private _titleService: Title,
     private _router: Router
-  ) {
+  ) { }
 
+  ngOnInit() {
     // смена title страницы
     this._router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        const pathTitle = this.getTitle(_router.routerState, _router.routerState.root).join(' • ');
+        const pathTitle = this._getTitle(this._router.routerState, this._router.routerState.root).join(' • ');
         const title = pathTitle.length === 0 ?
           this._baseUrlTitle :
           this._baseUrlTitle + ' • ' + pathTitle;
         this._titleService.setTitle(title);
       }
     });
-
   }
 
   /**
@@ -38,14 +38,14 @@ export class AppComponent {
    * @param state состояние роута
    * @param parent родительский роут
    */
-  getTitle(state: RouterState, parent: ActivatedRoute) {
+  private _getTitle(state: RouterState, parent: ActivatedRoute) {
     const data = [];
     if ( has(parent, 'snapshot.data.title') ) {
       data.push( this._translatePipe.transform(parent.snapshot.data.title) );
     }
 
     if (!!state && !!parent) {
-      data.push(... this.getTitle(state, (state as any).firstChild(parent)));
+      data.push(... this._getTitle(state, (state as any).firstChild(parent)));
     }
     return data;
   }
